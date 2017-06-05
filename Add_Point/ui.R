6f8bed7141e096207204c4323fdddcd6f670b74c
clarpaul <clarpaul@gmail.com>
2017-06-04 20:32:32 -0700

    Updated UI to display better on RStudio server

--- Add_Point/ui.R
+++ Add_Point/ui.R
@@ -15,34 +15,37 @@ shinyUI(fluidPage(
   titlePanel("Regression Line: Effect of Adding Observations"),
   
     # Show a plot and related information
-    mainPanel(
+       sidebarLayout(
+          sidebarPanel(  
+            # INSTRUCTIONS
+            span(h4("Instructions:"), style = "color:blue"), 
+            div("1. Click in plot area to add points to regression and see how the best-fit line changes."),
+            div("2. Use button to clear added points and start over"),
        
-       # INSTRUCTIONS
-       span(h4("Instructions:"), style = "color:blue"), 
-       div("1. Click in plot area to add points to regression and see how the best-fit line changes."),
-       div("2. Use button to clear added points and start over"),
+            
+          
+            # INFORMATION BOXES
+            h4("Coordinates of most recently added point"),
+            verbatimTextOutput("info_added_pt"),
+            h4("Equation: least squares best fit line"),
+            verbatimTextOutput("info_equation"),
+            h4("Correlation and p-value"),
+            verbatimTextOutput("info_coefs")
+          ),
+          mainPanel(
+            # PLOT
+            plotOutput("plot1", click = "plot1_click"),
+            
+            # BUTTON: CLEAR AND START OVER
+            br(),
+            actionButton("button", span("Clear added points", style = "color:blue")),
        
-       # BUTTON: CLEAR AND START OVER
-       br(),
-       actionButton("button", span("Clear added points", style = "color:blue")),
-       
-       # PLOT
-       plotOutput("plot1", click = "plot1_click"),
-       
-       # CHECKBOX: SHOW ORIGINAL TREND LINE
-       span(
-        checkboxInput("showOriginal", "Show Original Trend Line", value = TRUE),
-        style = "color:blue"
-       ),
-        
-       
-       # INFORMATION BOXES
-       h4("Coordinates of most recently added point"),
-       verbatimTextOutput("info_added_pt"),
-       h4("Equation: least squares best fit line"),
-       verbatimTextOutput("info_equation"),
-       h4("Correlation and p-value"),
-       verbatimTextOutput("info_coefs")
-    )
+            # CHECKBOX: SHOW ORIGINAL TREND LINE
+            span(
+              checkboxInput("showOriginal", "Show Original Trend Line", value = TRUE),
+              style = "color:blue"
+            )
+          )
+       )
   )
 )
